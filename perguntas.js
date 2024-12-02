2ee5cd177ffad6890476254a3420a42b3b5b06bc
cauapablo223 <160983834+cauapablo223@users.noreply.github.com>
2024-12-02 19:29:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'HTML (HyperText Markup Language) é a linguagem de marcação utilizada para criar páginas web.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continente do mundo é a Ásia.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Uma variável é um espaço na memória onde podemos armazenar dados que podem ser alterados durante a execução do programa.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz "bom dia" em inglês?',
+    '"Bom dia" em inglês é "Good morning".'
+)
